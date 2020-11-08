a73fbce5d24ede91df280eddc3b46f9978e2f4e7
Xiaohu-LeoLee <hackylee1314@gmail.com>
2020-11-08 11:34:35 +0800

    First Commit

---
+++ Assets/move.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class move : MonoBehaviour
+
+{
+
+    private Transform son;
+
+    public bool moveToLeft = true;
+
+    private float speed = 2;
+
+    private void Start()
+
+    {
+
+        son = this.transform;
+
+    }
+
+    private void Update()
+
+    {
+
+        Move();
+
+    }
+
+    private void Move()
+
+    {
+
+        if (son.position.x <= -3 && moveToLeft)
+
+        {
+
+            moveToLeft = false;
+
+        }
+
+        else if (son.position.x >= 3 && !moveToLeft)
+
+            moveToLeft = true;
+
+        son.position += (moveToLeft ? Vector3.left : Vector3.right) * Time.deltaTime * speed;
+
+    }
+
+}
